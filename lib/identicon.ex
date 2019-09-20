63067da172a118cd21a68d6e45fd35bb7c2eb86b
John Joseph <atiquajohn@gmail.com>
2019-09-20 13:08:24 +0700

    write more methods

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -1,18 +1,30 @@
 defmodule Identicon do
-  @moduledoc """
-  Documentation for Identicon.
-  """
+  def main(input) do
+    input
+    |> hash_input
+    |> pick_color
+    |> build_grid
+  end
 
-  @doc """
-  Hello world.
+  def build_grid(%Identicon.Image{hex: hex}) do
+    hex
+    |> Enum.chunk_every(3, 3, :discard)
+  end
 
-  ## Examples
+  def mirror_row(row) do
+    [first, second | _tail] = row
+    row ++ [first, second]
+  end
 
-      iex> Identicon.hello()
-      :world
+  def pick_color(%Identicon.Image{hex: [r, g, b | _tail]} = image) do
+    %Identicon.Image{image | color: {r, g, b}}
+  end
 
-  """
-  def hello do
-    :world
+  def hash_input(input) do
+    hex =
+      :crypto.hash(:md5, input)
+      |> :binary.bin_to_list()
+
+    %Identicon.Image{hex: hex}
   end
 end
